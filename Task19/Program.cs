4233479d04be5ef483975ac149c84db07237732b
belka.boo <ibelka.boo@gmail.com>
2022-10-29 08:00:36 +0300

    task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,56 +1,23 @@
-﻿//Task19
-// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 
-//Задание должно быть выполнено в методе. В методе не должно быть вывода в консоль. Не использовать String!
-//14212 -> нет
-//12821 -> да
-//23432 -> да
-
-Console.WriteLine("Input five-digit number: ");
-bool isNumber = int.TryParse(Console.ReadLine(), out int number);
+﻿Console.WriteLine("Input five-digit number: ");
+bool isNumber = int.TryParse(Console.ReadLine(), out int numberForCheck);
 if (!isNumber)
 {
 Console.WriteLine("Error. It's not a number");
     return;
 }
 
-int Digits(int number)
+
+bool CheckPalindrome(int number)
 {
-    int dig = 0;
-    for (dig = 0; number > 0; dig++)
+    int number1 = number;
+    int check = 0;
+    for (check = 0; number1 > 0;)
     {
-        number = number / 10;
+        check = (check * 10) + (number1 % 10);
+        number1 = number1 / 10;
     }
-
-    return dig;
-
+    
+    return check == number;
 }
 
-void FillArray(int[] inputArray, int num)
-{
-    int i = (inputArray.Length)- 1;
-    while (i >= 0)
-    {
-        inputArray[i] = num % 10;
-        num = num / 10;
-        i--;
-    }
-}
-
-bool CheckPolynDrom (int checkNumber)
-{
-    int[] array = new int[Digits(number)];
-    FillArray(array, number);
-    return array[0] == array[4] && array[1] == array[3];
-
-}
-
-bool check = CheckPolynDrom(number);
-if (check)
-{
-    Console.WriteLine("Yes");
-}
-else
-{
-    Console.WriteLine("No");
-}
-
+Console.WriteLine($"Is it palindrome? {CheckPalindrome(numberForCheck)}");
